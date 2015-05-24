e000d8ea4b6ef1ded707addc2624fce8d5457c1c
Tobias Schwedes <t.schwedes14@imperial.ac.uk>
2015-05-24 18:31:53 +0100

    new examples

--- opentidalfarm/fenics_reduced_functional.py
+++ opentidalfarm/fenics_reduced_functional.py
@@ -1,7 +1,9 @@
+import os.path
 from dolfin import *
 from dolfin_adjoint import *
 from solvers import Solver
 from functionals import TimeIntegrator, PrototypeFunctional
+import helpers
 
 __all__ = ["FenicsReducedFunctional"]
 
@@ -38,6 +40,12 @@ class FenicsReducedFunctional(ReducedFunctional):
 
         super(FenicsReducedFunctional, self).__init__(dolfin_adjoint_functional, controls)
 
+        # Caching variables that store which controls the last forward run was
+        # performed
+        self.last_m = None
+        if self.solver.parameters.dump_period > 0:
+            turbine_filename = os.path.join(solver.parameters.output_dir, "turbines.pvd")
+            self.turbine_file = File(turbine_filename, "compressed")
 
     def evaluate(self, annotate=True):
         """ Return the functional value for the given control values. """
@@ -71,18 +79,70 @@ class FenicsReducedFunctional(ReducedFunctional):
 
         return j
 
-    def derivative(self, forget=False, **kwargs):
+    def _update_turbine_farm(self):
+        """ Update the turbine farm from the flattened parameter array m. """
+        farm = self.solver.problem.parameters.tidal_farm
+
+        if farm.turbine_specification.smeared:
+            farm._parameters["friction"] = self.controls[0].data().vector()
+
+        #else:
+        #    controlled_by = farm.turbine_specification.controls
+        #    shift = 0
+        #    if controlled_by.friction:
+        #        shift = len(farm._parameters["friction"])
+        #        farm._parameters["friction"] = m[:shift]
+        #    elif controlled_by.dynamic_friction:
+        #        shift = len(numpy.reshape(farm._parameters["friction"],-1))
+        #        nb_turbines = len(farm._parameters["position"])
+        #        farm._parameters["friction"] = (
+        #            numpy.reshape(m[:shift], (-1, nb_turbines)).tolist())
+
+        #    if controlled_by.position:
+        #        m_pos = m[shift:]
+        #        farm._parameters["position"] = (
+        #            numpy.reshape(m_pos, (-1,2)).tolist())
+
+        # Update the farm cache.
+        farm.update()
+
+    def derivative(self, forget=False, new_optimisation_iteration=True, **kwargs):
         """ Computes the first derivative of the functional with respect to its
         controls by solving the adjoint equations. """
-
+        #self.evaluate()
         log(INFO, 'Start evaluation of dj')
         timer = dolfin.Timer("dj evaluation")
 
-        #J = self.time_integrator.dolfin_adjoint_functional()
-        J = self.functional
+        J = self.time_integrator.dolfin_adjoint_functional(self.solver.state)
+        #J = self.functional
         dj = compute_gradient(J, self.controls, forget=forget, **kwargs)
         dolfin.parameters["adjoint"]["stop_annotating"] = False
 
         log(INFO, "Runtime: " + str(timer.stop()) + " s")
 
-        return dj
+        # We assume that the gradient is computed at and only at the beginning
+        # of each new optimisation iteration. Hence, this is the right moment
+        # to store the turbine friction field and to increment the optimisation
+        # iteration counter.
+        if new_optimisation_iteration:
+            self.solver.optimisation_iteration += 1
+            farm = self.solver.problem.parameters.tidal_farm
+            self._update_turbine_farm()
+
+            if (self.solver.parameters.dump_period > 0 and
+                farm is not None):
+                # A cache hit skips the turbine cache update, so we need
+                # trigger it manually.
+                #if self._compute_gradient_mem.has_cache(m, forget):
+                #    self._update_turbine_farm(m)
+                #if farm.turbine_specification.controls.dynamic_friction:
+                #    log(WARNING, ("Turbine VTU output not yet implemented for "
+                #                  " dynamic turbine control"))
+                #else:
+                    self.turbine_file << farm.turbine_cache['turbine_field']
+                    # Compute the total amount of friction due to turbines
+                    if farm.turbine_specification.smeared:
+                        log(INFO, "Total amount of friction: %f" %
+                            assemble(farm.turbine_cache["turbine_field"]*dx))
+        return dj 
+
