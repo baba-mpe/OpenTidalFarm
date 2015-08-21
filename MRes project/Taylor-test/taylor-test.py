04e67ee55af2b5b672e040f80b7008535ee6dfec
Tobias Schwedes <t.schwedes14@imperial.ac.uk>
2015-08-21 18:49:44 +0100

    taylor-test

--- MRes project/Taylor-test/taylor-test.py
+++ MRes project/Taylor-test/taylor-test.py
@@ -31,48 +31,53 @@ turbine = SmearedTurbine()
 V = FunctionSpace(domain.mesh, "CG", 3)
 
 # Define reduced functional
-def Jhat(turbine_density):
+def Jhat(friction_function):
     farm = Farm(domain, turbine, function_space=V, site_ids=1)
-    farm.turbine_density.assign(turbine_density)
+    farm.friction_function.assign(friction_function)
     prob_params.tidal_farm = farm
     problem = SteadySWProblem(prob_params)
     sol_params = CoupledSWSolver.default_parameters()
     sol_params.dump_period = 1
     solver = CoupledSWSolver(problem, sol_params)
     functional = PowerFunctional(problem)
-    control = Control(farm.turbine_density) 
+    control = Control(farm.friction_function) 
     rf = FenicsReducedFunctional(functional, control, solver) 
     return rf.evaluate()
 
 # Define derivative of reduced functional
-def dJhat(turbine_density):
+def dJhat(friction_function):
     farm = Farm(domain, turbine, function_space=V, site_ids=1)
-    farm.turbine_density.assign(turbine_density)
+    farm.friction_function.assign(friction_function)
     prob_params.tidal_farm = farm
     problem = SteadySWProblem(prob_params)
     sol_params = CoupledSWSolver.default_parameters()
     sol_params.dump_period = 1
     solver = CoupledSWSolver(problem, sol_params)
     functional = PowerFunctional(problem)
-    control = Control(farm.turbine_density) 
+    control = Control(farm.friction_function) 
     rf = FenicsReducedFunctional(functional, control, solver) 
     return rf.derivative()
 
 # Friction function 1
-turbine_density = Constant(0.1)
+#friction_function = Constant(0.03)
 # Friction function 2
-turbine_density = interpolate(Expression("1e-05*(1 + x[0]*x[0] + 2*x[1]*x[1])"),V)
-# Fricyion function 3
-turbine_density = interpolate(Expression("1e-02*(sin(2*pi*x[0]) - cos(2*pi*x[1]))"), V)
+#friction_function = interpolate(Expression("1e-08*(1 + x[0]*x[0] + 2*x[1]*x[1])"),V)
+# Friction function 3
+friction_function = interpolate(Expression("1e-02*(2+ sin(2*pi*x[0]) - cos(2*pi*x[1]))"), V)
+
 
 # Set control to turbine density
 farm = Farm(domain, turbine, function_space=V, site_ids=1)
-farm.turbine_density.assign(turbine_density)
-control = Control(farm.turbine_density) 
+
+#import ipdb; ipdb.set_trace() 
+
+
+farm.friction_function.assign(friction_function)
+control = Control(farm.friction_function) 
 
 # Compute reduced funtional and its derivative
-J_dens = Jhat(turbine_density) 
-dJ_dens = dJhat(turbine_density)
+J_dens = Jhat(friction_function) 
+dJ_dens = dJhat(friction_function)
 
 # Compute convergence rates using imported method from dolfin_adjoint
 conv_rate = taylor_test(Jhat, control, J_dens, dJ_dens) 
